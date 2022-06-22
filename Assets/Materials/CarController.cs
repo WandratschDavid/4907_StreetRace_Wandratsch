623674513f07ae2bbeca78a772e685dab0450b6c
WandratschDavid <72546181+David291202@users.noreply.github.com>
2022-06-23 00:47:18 +0200

    implemented main part of the game

---
+++ Assets/Materials/CarController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CarController : MonoBehaviour
+{
+    public float fuel = 1;
+    public int score = 0;    
+    public float fuelConsumption = 0.1f;
+    public Rigidbody2D carRigidBody;
+    public Rigidbody2D backTire;
+    public Rigidbody2D frontTire;
+
+    private float movement;
+    public float speed = 100;
+    public float carTorque = 10;
+
+    public UnityEngine.UI.Image image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontal");
+        image.fillAmount = fuel;
+    }
+
+    private void FixedUpdate()
+    {
+        if(fuel > 0)
+        {
+            backTire.AddTorque(-movement * speed * Time.fixedDeltaTime);
+            frontTire.AddTorque(-movement * speed * Time.fixedDeltaTime);
+            carRigidBody.AddTorque(-movement * carTorque * Time.fixedDeltaTime);
+        }
+        else if(fuel < 1) 
+        {
+            SceneManager.LoadScene("Menu");
+        }
+
+        fuel -= fuelConsumption * Mathf.Abs(movement) * Time.fixedDeltaTime; 
+    }
+}
